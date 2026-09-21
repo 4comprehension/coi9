41eabe9c6feb8328a2a047bbb961a8100dd205e0
4Comprehension <4comprehension@gmail.com>
2026-09-21 11:14:25 +0200

    t1

--- rental-store/src/main/java/com/pivovarit/movies/Application.java
+++ rental-store/src/main/java/com/pivovarit/movies/Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+    // 1. autowire the whole thing
+    // 2. GET /movies/{id}
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
