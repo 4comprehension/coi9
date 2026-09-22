0e5b9696b2f4639ea4588bd44a98e22368cbc0d9
4Comprehension <4comprehension@gmail.com>
2026-09-22 09:23:56 +0200

    7. movie summaries (exercise)

--- rental-store/src/main/java/com/pivovarit/Application.java
+++ rental-store/src/main/java/com/pivovarit/Application.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+    // 1. /movies oraz /movies/{id} zwracają dodatkowo 'summary' (rozszerzamy MovieDto)
+    // 2. SummaryFacade wykorzystujemy w RentalFacade
+    // 3. Robimy update unit oraz black-box tests
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
---
+++ rental-store/src/main/java/com/pivovarit/modules/summary/SummaryFacade.java
@@ -0,0 +1,10 @@
+package com.pivovarit.modules.summary;
+
+import java.util.Optional;
+
+public final class SummaryFacade {
+
+    Optional<String> getSummary(int movieId) {
+        throw new RuntimeException("not implemented!");
+    }
+}
