3806ac8ba3f564d42df93be7d40cff7716a325cb
4Comprehension <4comprehension@gmail.com>
2026-09-23 10:33:53 +0200

    18. outbox (exercise)

--- rental-store/src/main/java/com/pivovarit/Application.java
+++ rental-store/src/main/java/com/pivovarit/Application.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+    // 1. dodać tabelę outbox do movie-summaries, która będzie robiła za 'skrzynkę nadawczą' z eventami
+    // checkpoint (dyskutujemy)
+    // 2. zapis w jednej tx do tabeli z opisami oraz outbox (+ blackbox test)
+    // checkpoint (dyskutujemy)
+    // 3. event processor, który wyłapuje eventy i wrzuca do rabbitmq
+    // checkpoint (dyskutujemy)
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
